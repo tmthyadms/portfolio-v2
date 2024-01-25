cc6cfb260a40501f907c92626028d5ecc74b4fbc
Timothy Adams <timothyadamsjenuil@gmail.com>
2024-01-26 01:06:48 +0800

    feat: add socials

--- components/the-footer.vue
+++ components/the-footer.vue
@@ -1,13 +1,41 @@
+<script setup lang="ts">
+import socials from "@/assets/data/socials.json";
+</script>
+
 <template>
   <footer>
     <prime-divider align="center">
       <app-logo :short="true" />
     </prime-divider>
     <div
-      class="flex justify-center px-4 py-6 text-center text-muted sm:px-6 lg:px-8"
+      class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:flex lg:items-center lg:justify-between lg:px-8 lg:py-4"
     >
-      <icon name="mdi:star-outline" size="1.25rem" />
-      <span>&nbsp;<time>2024</time>&nbsp;Timothy Adams</span>
+      <div class="flex justify-center gap-x-1.5 lg:order-2">
+        <template v-for="social in socials">
+          <a
+            v-ripple
+            :href="social.to"
+            target="_blank"
+            class="p-ripple p-button p-button-sm p-button-square p-button-text p-button-plain p-button-icon-only"
+          >
+            <icon :name="social.icon" size="1.25rem" />
+          </a>
+        </template>
+      </div>
+      <div class="mt-3 flex justify-center lg:order-1 lg:mt-0 lg:justify-start">
+        <p class="text-sm text-content">
+          <icon
+            name="ph:star-duotone"
+            class="mb-0.5"
+          />&nbsp;<time>2024</time>&nbsp;<span>Timothy Adams</span>
+        </p>
+      </div>
     </div>
   </footer>
 </template>
+
+<style scoped>
+.p-button {
+  @apply shadow-none;
+}
+</style>
