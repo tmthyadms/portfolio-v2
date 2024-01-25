932ce1cc3020ea8d72a7697ab3b60f2132d64895
Timothy Adams <timothyadamsjenuil@gmail.com>
2024-01-26 01:19:10 +0800

    feat: update carousel navigation buttons and indicators icons

--- components/hero-projects.vue
+++ components/hero-projects.vue
@@ -30,6 +30,13 @@ function updateOrientation(mobileMedia: MediaQueryList | MediaQueryListEvent) {
   const isMobile = mobileMedia.matches;
   orientation.value = isMobile ? Orientation.Vertical : Orientation.Horizontal;
 }
+const isVertical = computed(() => orientation.value === Orientation.Vertical);
+const nextIcon = computed(() =>
+  isVertical.value ? "ph:arrow-down" : "ph:arrow-right",
+);
+const previousIcon = computed(() =>
+  isVertical.value ? "ph:arrow-up" : "ph:arrow-left",
+);
 </script>
 
 <template>
@@ -39,9 +46,9 @@ function updateOrientation(mobileMedia: MediaQueryList | MediaQueryListEvent) {
         v-ripple
         href="https://github.com/tmthyadms?tab=repositories"
         target="_blank"
-        class="p-ripple p-button p-button-sm p-button-secondary p-button-outlined"
+        class="p-ripple p-button p-button-contrast"
       >
-        <icon name="mdi:folder-multiple-outline" size="1rem" />
+        <icon name="ph:folders-duotone" size="1.25rem" />
         See all projects
       </a>
     </template>
@@ -51,27 +58,60 @@ function updateOrientation(mobileMedia: MediaQueryList | MediaQueryListEvent) {
       :num-scroll="1"
       :responsive-options="responsiveOptions"
       :orientation="orientation"
-      vertical-view-port-height="480px"
+      vertical-view-port-height="490px"
       :autoplay-interval="3000"
       circular
       class="max-w-80 sm:max-w-xl lg:max-w-screen-lg 2xl:max-w-screen-2xl"
     >
       <template #item="{ data }">
         <card-project
-          :image="data.image"
           :title="data.title"
           :subtitle="data.subtitle"
           :description="data.description"
+          :category="data.category"
+          :image="data.image"
           :to="data.to"
           class="m-2"
         />
+      </template>
+      <template #nexticon>
+        <icon :name="nextIcon" size="1.25rem" />
+      </template>
+      <template #previousicon>
+        <icon :name="previousIcon" size="1.25rem" />
       </template>
     </prime-carousel>
   </app-hero>
 </template>
 
 <style scoped>
+:deep(.p-carousel-prev),
+:deep(.p-carousel-next) {
+  @apply ring-surface-200 dark:ring-surface-100 bg-surface-b ring-1 ring-inset dark:bg-surface-c;
+}
+
+:deep(.p-carousel-prev:hover),
+:deep(.p-carousel-next:hover) {
+  @apply bg-primary/10 text-primary ring-primary ring-opacity-50;
+}
+
+:deep(.p-carousel-indicators) {
+  @apply space-x-2;
+}
+
+:deep(.p-carousel-indicator) {
+  @apply m-0;
+}
+
+:deep(.p-carousel-indicator button) {
+  @apply h-2 w-2 rounded-full;
+}
+
 :deep(.p-carousel-item) {
   @apply flex;
+}
+
+:deep(.p-card-footer) {
+  @apply mt-auto p-0;
 }
 </style>
