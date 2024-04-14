35de03849642c17b6ebeae38512598e91b7c3af9
Timothy Adams <timothyadamsjenuil@gmail.com>
2024-04-15 01:10:18 +0800

    feat: implement exp reusability and inplace for exp desc on mobile

--- components/hero-experience.vue
+++ components/hero-experience.vue
@@ -1,26 +1,68 @@
+<script setup lang="ts">
+import jobs from "@/assets/data/experience.json";
+
+const inplace = ref(false);
+</script>
+
 <template>
   <app-hero title="Experience" description="See where I've worked.">
-    <app-card
-      v-animateonscroll.once="{ enterClass: 'fadein', leaveClass: 'fadeout' }"
-      once
-      icon="ph:briefcase-duotone"
-      title="Frontend Support"
-      subtitle="Benefit Solutions Asia Sdn Bhd"
-      class="duration-1000"
-    >
-      <template #content>
-        <div class="space-y-1.5 text-[15px]">
-          <ul class="list-inside list-disc">
-            <li>Resolved front-end issues.</li>
-            <li>Developed new components.</li>
-            <li>Built an end-to-end (E2E) automated testing.</li>
-          </ul>
-          <p><time>Duration: 6 months</time></p>
-        </div>
-      </template>
-      <template #footer>
-        <app-chip label="Internship" />
-      </template>
-    </app-card>
+    <template v-for="job in jobs">
+      <app-card
+        class="transition-opacity duration-1000 max-w-xl mx-4 sm:mx-auto"
+        v-animateonscroll.once="{ enterClass: 'fadein', leaveClass: 'fadeout' }"
+        once
+        :icon="!job.resigned ? 'ph:briefcase-duotone' : ''"
+        :title="job.role.name"
+      >
+        <template #subtitle>
+          <p class="mb-1">{{ job.company }}</p>
+          <p>
+            <time datetime="2023-08">{{ job.date.start }}</time>
+            -
+            <time datetime="2024-01">{{ job.date.end }}</time>
+          </p>
+        </template>
+        <template #content>
+          <div class="text-[15px] text-content prose">
+            <prime-button
+              class="w-full sm:hidden"
+              :class="{
+                'inline-flex': !inplace,
+                hidden: inplace,
+              }"
+              size="small"
+              severity="secondary"
+              outlined
+              @click="inplace = true"
+            >
+              <icon class="text-primary" name="ph:bookmark-simple-duotone" />
+              Read more about this job
+            </prime-button>
+            <ul class="sm:block" :class="{ hidden: !inplace }">
+              <li v-for="item in job.description">
+                {{ item }}
+              </li>
+            </ul>
+          </div>
+        </template>
+        <template #footer>
+          <app-chip :label="job.role.type" />
+        </template>
+      </app-card>
+    </template>
   </app-hero>
 </template>
+
+<style scoped>
+.hero {
+  @apply content-center justify-normal;
+}
+
+:deep(.hero-content) {
+  @apply w-full space-y-8;
+}
+
+:deep(.p-button-outlined) {
+  @apply border border-dashed border-surface-200 ring-0 dark:border-surface-100;
+}
+</style>
